531a873decdc9be7a49ebcc4e7dd2b864ba4aff7
NazarovErnar <ernar13.06.2005@gmail.com>
2026-02-13 20:34:54 +0500

    lab2

--- lab2/fclass1.java
+++ lab2/fclass1.java
@@ -6,4 +6,21 @@ public class fclass1 {
     public static void main(String[] args) {
         fclass3 obj = new fclass3();
         obj.show();
+    }
+}
+
+class fclass2 extends fclass1 {
+    @Override
+    public void show() {
+        super.show(); // fclass1
+        System.out.println("Это класс fclass2");
+    }
+}
+
+class fclass3 extends fclass2 {
+    @Override
+    public void show() {
+        super.show(); // fclass1 + fclass2
+        System.out.println("Это класс fclass3");
+    }
 }
