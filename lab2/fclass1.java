fcaf539cf5931f274f489020dbbd548926a131c2
NazarovErnar <ernar13.06.2005@gmail.com>
2026-02-13 20:01:08 +0500

    lab2

--- lab2/fclass1.java
+++ lab2/fclass1.java
@@ -1,14 +1,24 @@
-class fclass {
+class fclass1 {
 
-    protected int number = 100;
+    void show() {
+        System.out.println("Бұл fclass1 класы");
+    }
 }
 
-class fclass2 extends fclass {
+class fclass2 extends fclass1 {
+
+    void info() {
+        System.out.println("Бұл fclass2 класы");
+    }
+}
+
+class fclass3 extends fclass2 {
 
     public static void main(String[] args) {
 
-        fclass2 obj = new fclass2();
+        fclass3 obj = new fclass3();
 
-        System.out.println("Protected айнымалы мәні: " + obj.number);
+        obj.show();  // fclass1 әдісі
+        obj.info();  // fclass2 әдісі
     }
 }
