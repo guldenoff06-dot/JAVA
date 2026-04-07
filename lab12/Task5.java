8c1d6ffa8c29de2b5681e5b1da436aabefe212be
gulden <guldenoff06@icloud.com>
2026-04-07 11:14:30 +0500

    lab12

---
+++ lab12/Task5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String text = sc.nextLine();
+        String word = sc.nextLine();
+
+        System.out.println(text.startsWith(word));
+    }
+}
+
+
---
+++ lab12/Task9.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+
+        String result = text.replaceAll("[aeiouAEIOUаәеёиоуыі]", "*");
+        System.out.println(result);
+    }
+}
+
+
+
